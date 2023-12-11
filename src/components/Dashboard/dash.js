8b1585218cad2b7c3667da1a5bbf90ee3f3a191a
Rudraneel Dutta <its.rudraneel@gmail.com>
2023-12-11 20:42:29 +0530

    logo stuff

--- src/components/Dashboard/dash.js
+++ src/components/Dashboard/dash.js
@@ -5,30 +5,53 @@ const Home = () => {
   return (
     <div className="dash">
       <div className="form-zone">
-      <form className='input-form'>
+        <form className="input-form">
           <p>
             <input type="text" id="name" placeholder="Name" required />
           </p>
           <p>
-            <input type="number" id="rollNumber" placeholder="Roll number" required />
+            <input
+              type="number"
+              id="rollNumber"
+              placeholder="Roll number"
+              required
+            />
           </p>
           <p>
             <select name="year" id="year" required>
-              <option value="" selected disabled hidden>Choose Year</option>
+              <option value="" selected disabled hidden>
+                Choose Year
+              </option>
               <option value="2nd">2nd year</option>
               <option value="3rd">3rd year</option>
             </select>
           </p>
           <p>
-            <input type="number" id="currentSection" placeholder="Current Section" min="1" max="99" required />
+            <input
+              type="number"
+              id="currentSection"
+              placeholder="Current Section"
+              min="1"
+              max="99"
+              required
+            />
           </p>
           <p>
-            <input type="number" id="requiredSection" placeholder="Required Section" min="1" max="99" required />
+            <input
+              type="number"
+              id="requiredSection"
+              placeholder="Required Section"
+              min="1"
+              max="99"
+              required
+            />
           </p>
           <button type="submit">Submit</button>
         </form>
       </div>
-      <img className="bg-container" src={HomeBg} alt="homeBg" />
+      <div className="bg-container">
+        <img className="form-image" src={HomeBg} alt="homeBg" />
+      </div>
     </div>
   )
 }
